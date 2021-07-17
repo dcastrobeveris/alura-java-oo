4ee6a24aa23c85e432f7c9275941c428827c7641
Diego Barcellos <dcastrob@everis.com>
2021-07-17 09:21:24 -0300

    Métodos com referência e mais retorno

--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -9,6 +9,17 @@ public class TestaMetodo {
 		boolean conseguiuRetirar = contaDoDiego.saca(20);
 		System.out.println(contaDoDiego.saldo);
 		System.out.println(conseguiuRetirar);
+		
+		Conta contaDaAna = new Conta();
+		contaDaAna.deposita(1000);
+		boolean sucessoTransferencia = contaDaAna.transfere(300, contaDoDiego);
+		if(sucessoTransferencia) {
+			System.out.println("Transferência realizada com sucesso");
+		} else {
+			System.out.println("Faltou dinheiro");
+		}
+		System.out.println(contaDaAna.saldo);
+		System.out.println(contaDoDiego.saldo);
 	}
 
 }
