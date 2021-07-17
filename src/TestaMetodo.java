a9a00cd357353201af04ab0dc9052606e1ade50b
Diego Barcellos <dcastrob@everis.com>
2021-07-17 08:38:20 -0300

    Métodos com retorno

--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -6,6 +6,9 @@ public class TestaMetodo {
 		contaDoDiego.saldo = 100;
 		contaDoDiego.deposita(50);
 		System.out.println(contaDoDiego.saldo);
+		boolean conseguiuRetirar = contaDoDiego.saca(20);
+		System.out.println(contaDoDiego.saldo);
+		System.out.println(conseguiuRetirar);
 	}
 
 }
